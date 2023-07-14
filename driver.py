fb81bf0696222543972bf68576d44beca52d8189
JoahanC <joahanc@github.com>
2023-07-14 14:15:46 -0700

    Updated current running script, issues with replicating results found in local machine

--- driver.py
+++ driver.py
@@ -4,5 +4,5 @@ seqid = "30501002002"
 path = f"../data/{seqid}/"
 evdir = f"{path}event_cl/"
 out_path = f"{path}products/"
-test = NuAnalysis(2000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True)
+test = NuAnalysis(1000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True)
 test.generate_detections()
