fed31f570e3c70da7e0bfa290af517353261e58d
JoahanC <joahanc@github.com>
2023-07-25 09:37:33 -0700

    Adding server script

--- test.py
+++ test.py
@@ -4,12 +4,16 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 import glob
 
+seqids = []
+data_dirs = (glob.glob("./../data/*"))
+for dir in data_dirs:
+    seqids.append(dir.replace("./../data/", ''))
 
-seqid = "30501002002"
-path = f"../data/test/{seqid}/"
-evdir = f"{path}event_cl/"
-out_path = f"{path}products/"
-test = NuAnalysis(10000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=False)
-test.test_gen()
-test.extract_detections()
-remove_tmp_folders(path)
+for seqid in seqids:
+    path = f"../data/{seqid}/"
+    evdir = f"{path}event_cl/"
+    out_path = f"{path}products/"
+    test = NuAnalysis(10000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=False)
+    test.test_gen()
+    test.extract_detections()
+    remove_tmp_folders(path)
