904ea4034208e627ca3d32c6b2bb14e956324082
JoahanC <joahanc@github.com>
2023-08-04 07:49:02 -0700

    Server script

--- bifrost_1.py
+++ bifrost_1.py
@@ -10,8 +10,12 @@ for idx, datum in enumerate(run_data):
     run_order[idx] = datum.split()
 
 for idx in run_order:
+    object_name = run_order[idx][0]
+    seqid = run_order[idx][1]
+    
+    
     path = f"../bifrost_data/1/{seqid}/"
     evdir = f"{path}event_cl/"
     out_path = f"{path}products/"
-    run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=False, bifrost=True)
+    run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=False, bifrost=True, object_name=object_name)
 
