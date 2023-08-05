781fe2669a2d21428978c66a870592281f1957f6
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-04 20:50:13 -0700

    Modifiying bifrost scripts for deployment

--- bifrost_7.py
+++ bifrost_7.py
@@ -18,3 +18,34 @@ for idx in run_order:
     out_path = f"{path}products/"
     run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=False, bifrost=True, object_name=object_name, nupath=nupath)
 
+run_order = {}
+counter = 0
+with open("../test/runlist_17.txt", 'r') as run_file:
+    run_data = run_file.readlines()
+for idx, datum in enumerate(run_data):
+    run_order[idx] = datum.split()
+
+for idx in run_order:
+    object_name = run_order[idx][0]
+    seqid = run_order[idx][1]
+    path = f"../bifrost_data/17/{seqid}/"
+    nupath = f"./1/{seqid}/"
+    evdir = f"{path}event_cl/"
+    out_path = f"{path}products/"
+    run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=False, bifrost=True, object_name=object_name, nupath=nupath)
+
+run_order = {}
+counter = 0
+with open("../test/runlist_27.txt", 'r') as run_file:
+    run_data = run_file.readlines()
+for idx, datum in enumerate(run_data):
+    run_order[idx] = datum.split()
+
+for idx in run_order:
+    object_name = run_order[idx][0]
+    seqid = run_order[idx][1]
+    path = f"../bifrost_data/27/{seqid}/"
+    nupath = f"./1/{seqid}/"
+    evdir = f"{path}event_cl/"
+    out_path = f"{path}products/"
+    run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=False, bifrost=True, object_name=object_name, nupath=nupath)
