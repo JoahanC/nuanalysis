63f897410e48b61c87be2d5cea3ec9d3f87fe70b
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-07 12:25:25 -0700

    Sliding cell trial

--- bifrost_8.py
+++ bifrost_8.py
@@ -12,13 +12,12 @@ for idx, datum in enumerate(run_data):
 for idx in run_order:
     object_name = run_order[idx][0]
     seqid = run_order[idx][1]
-    if f"nu{seqid}A01_cl.evt" in os.listdir(f"../bifrost_data/8/{seqid}/event_cl/"):
-        if f"nu{seqid}B01_cl.evt" in os.listdir(f"../bifrost_data/8/{seqid}/event_cl/"):
-            path = f"../bifrost_data/8/{seqid}/"
-            evdir = f"{path}event_cl/"
-            out_path = f"{path}products/"
-            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=8)
-            run_object.event_extraction()
+    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/8/{seqid}/event_cl/"):
+        path = f"../bifrost_data/8/{seqid}/"
+        evdir = f"{path}event_cl/"
+        out_path = f"{path}products/"
+        run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=8)
+        run_object.sliding_cell_detection()
 
 run_order = {}
 counter = 0
@@ -30,13 +29,12 @@ for idx, datum in enumerate(run_data):
 for idx in run_order:
     object_name = run_order[idx][0]
     seqid = run_order[idx][1]
-    if f"nu{seqid}A01_cl.evt" in os.listdir(f"../bifrost_data/18/{seqid}/event_cl/"):
-        if f"nu{seqid}B01_cl.evt" in os.listdir(f"../bifrost_data/18/{seqid}/event_cl/"):
-            path = f"../bifrost_data/18/{seqid}/"
-            evdir = f"{path}event_cl/"
-            out_path = f"{path}products/"
-            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=18)
-            run_object.event_extraction()
+    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/18/{seqid}/event_cl/"):
+        path = f"../bifrost_data/18/{seqid}/"
+        evdir = f"{path}event_cl/"
+        out_path = f"{path}products/"
+        run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=18)
+        run_object.sliding_cell_detection()
 
 run_order = {}
 counter = 0
@@ -48,10 +46,9 @@ for idx, datum in enumerate(run_data):
 for idx in run_order:
     object_name = run_order[idx][0]
     seqid = run_order[idx][1]
-    if f"nu{seqid}A01_cl.evt" in os.listdir(f"../bifrost_data/28/{seqid}/event_cl/"):
-        if f"nu{seqid}B01_cl.evt" in os.listdir(f"../bifrost_data/28/{seqid}/event_cl/"):
-            path = f"../bifrost_data/28/{seqid}/"
-            evdir = f"{path}event_cl/"
-            out_path = f"{path}products/"
-            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=28)
-            run_object.event_extraction()
+    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/28/{seqid}/event_cl/"):
+        path = f"../bifrost_data/28/{seqid}/"
+        evdir = f"{path}event_cl/"
+        out_path = f"{path}products/"
+        run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=28)
+        run_object.sliding_cell_detection()
