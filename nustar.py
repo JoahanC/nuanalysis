c40d291f3da16f259ed551007ca860c3b5ccee0d
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-07 03:36:04 -0700

    fix?

--- nustar.py
+++ nustar.py
@@ -386,10 +386,7 @@ class Observation():
                         else:
                             self._exposure[keystr] += hdr['EXPOSURE']
         evt_array = getdata(self.science_files[mod][0])
-        counter = 0
         for evt in evt_array:
-            print(counter)
-            counter += 1
             self._event_times.append(evt[0])
         
         return
