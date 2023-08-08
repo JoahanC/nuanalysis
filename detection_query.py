222f88b36d704214701a49852451d137736c1baf
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-08 14:44:23 -0700

    updated type

--- detection_query.py
+++ detection_query.py
@@ -6,9 +6,11 @@ from tqdm import tqdm
 
 
 total_table = Table.read("complete.tbl", format='ipac')
+print(len(total_table['RA']))
 ra_vals = total_table['RA']
-radius = 32 * u.arcsecond
+radius = 13 * u.arcsecond
 for idx in tqdm(range(len(total_table['RA']))):
-    pos = SkyCoord(f"{total_table['RA'][idx]} {total_table['DEC'][idx]}", unit=(u.hourangle, u.deg))
-    result_table = Vizier.query_region(pos, radius=radius)
-    print(result_table)
+    pos = SkyCoord(f"{total_table['RA'][idx]} {total_table['DEC'][idx]}", unit=(u.hourangle, u.deg), frame='fk5')
+    print(float(total_table['PROB'][idx]))
+    #result_table = Vizier.query_region(pos, radius=radius)
+    #print(result_table)
