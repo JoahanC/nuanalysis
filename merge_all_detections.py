6f5197c99a93160791fcad2af837578d7151d4da
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-08 13:59:10 -0700

    fix idx

--- merge_all_detections.py
+++ merge_all_detections.py
@@ -9,7 +9,7 @@ from helpers import *
 from astropy.io import fits
 from astropy import units as u
 from astropy.wcs import WCS
-from astropy.table import QTable, Table, Column
+from astropy.table import QTable, Table, Column, vstack
 from tqdm import tqdm
 import radial_profile
 from astropy.coordinates import SkyCoord
@@ -18,4 +18,8 @@ from regions import CircleSkyRegion
 
 basepath = "./../bifrost_data/**/**/detections/5000.tbl"
 table_files = glob.glob(basepath)
-print(len(table_files))
+total_table = Table.read(table_files[0], formate='ipac')
+for file in table_files[1:]:
+    data_table = Table.read(file, format='ipac')
+    total_table = vstack(total_table, data_table)
+total_table.write("complete.tbl", format='ipac')
