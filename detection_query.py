6ad9afaa48f6863be60c11211800e5d3f7a1840f
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-08 14:24:47 -0700

    query test

---
+++ detection_query.py
@@ -0,0 +1,13 @@
+from astropy.table import QTable, Table, Column, vstack
+from astroquery.vizier import Vizier
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from tqdm import tqdm
+
+
+total_table = Table.read("complete.tbl", format='ipac')
+ra_vals = total_table['RA']
+radius = 32 * u.arcsecond
+for idx in tqdm(range(len(total_table['RA']))):
+    pos = SkyCoord(f"{total_table['RA'][idx]} {total_table['DEC'][idx]}", unit=(u.hourangle, u.deg))
+    result_table = Vizier.query_region(pos, radius=radius)
