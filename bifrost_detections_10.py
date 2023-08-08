f921e4d6ae0acb74e6ba4a3249e6227ae77b9dc1
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-08 10:22:10 -0700

    added detectio merging scripts

---
+++ bifrost_detections_10.py
@@ -0,0 +1,58 @@
+from helpers import *
+from nuanalysis import NuAnalysis
+
+
+run_order = {}
+counter = 0
+with open("../test/runlist_10.txt", 'r') as run_file:
+    run_data = run_file.readlines()
+for idx, datum in enumerate(run_data):
+    run_order[idx] = datum.split()
+
+for idx in run_order:
+    object_name = run_order[idx][0]
+    seqid = run_order[idx][1]
+    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/10/{seqid}/event_cl/"):
+        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/10/{seqid}/event_cl/"):
+            path = f"../bifrost_data/10/{seqid}/"
+            evdir = f"{path}event_cl/"
+            out_path = f"{path}products/"
+            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=10)
+            run_object.detection_merging()
+
+run_order = {}
+counter = 0
+with open("../test/runlist_20.txt", 'r') as run_file:
+    run_data = run_file.readlines()
+for idx, datum in enumerate(run_data):
+    run_order[idx] = datum.split()
+
+for idx in run_order:
+    object_name = run_order[idx][0]
+    seqid = run_order[idx][1]
+    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/20/{seqid}/event_cl/"):
+        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/20/{seqid}/event_cl/"):
+            path = f"../bifrost_data/20/{seqid}/"
+            evdir = f"{path}event_cl/"
+            out_path = f"{path}products/"
+            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=20)
+            run_object.detection_merging()
+
+run_order = {}
+counter = 0
+with open("../test/runlist_30.txt", 'r') as run_file:
+    run_data = run_file.readlines()
+for idx, datum in enumerate(run_data):
+    run_order[idx] = datum.split()
+
+for idx in run_order:
+    object_name = run_order[idx][0]
+    seqid = run_order[idx][1]
+    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/30/{seqid}/event_cl/"):
+        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/30/{seqid}/event_cl/"):
+            path = f"../bifrost_data/30/{seqid}/"
+            evdir = f"{path}event_cl/"
+            out_path = f"{path}products/"
+            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=30)
+            run_object.detection_merging()
+
---
+++ bifrost_detections_2.py
@@ -0,0 +1,57 @@
+from helpers import *
+from nuanalysis import NuAnalysis
+
+
+run_order = {}
+counter = 0
+with open("../test/runlist_2.txt", 'r') as run_file:
+    run_data = run_file.readlines()
+for idx, datum in enumerate(run_data):
+    run_order[idx] = datum.split()
+
+for idx in run_order:
+    object_name = run_order[idx][0]
+    seqid = run_order[idx][1]
+    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/2/{seqid}/event_cl/"):
+        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/2/{seqid}/event_cl/"):
+            path = f"../bifrost_data/2/{seqid}/"
+            evdir = f"{path}event_cl/"
+            out_path = f"{path}products/"
+            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=2)
+            run_object.detection_merging()
+
+run_order = {}
+counter = 0
+with open("../test/runlist_12.txt", 'r') as run_file:
+    run_data = run_file.readlines()
+for idx, datum in enumerate(run_data):
+    run_order[idx] = datum.split()
+
+for idx in run_order:
+    object_name = run_order[idx][0]
+    seqid = run_order[idx][1]
+    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/12/{seqid}/event_cl/"):
+        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/12/{seqid}/event_cl/"):
+            path = f"../bifrost_data/12/{seqid}/"
+            evdir = f"{path}event_cl/"
+            out_path = f"{path}products/"
+            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=12)
+            run_object.detection_merging()
+
+run_order = {}
+counter = 0
+with open("../test/runlist_22.txt", 'r') as run_file:
+    run_data = run_file.readlines()
+for idx, datum in enumerate(run_data):
+    run_order[idx] = datum.split()
+
+for idx in run_order:
+    object_name = run_order[idx][0]
+    seqid = run_order[idx][1]
+    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/22/{seqid}/event_cl/"):
+        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/22/{seqid}/event_cl/"):
+            path = f"../bifrost_data/22/{seqid}/"
+            evdir = f"{path}event_cl/"
+            out_path = f"{path}products/"
+            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=22)
+            run_object.detection_merging()
