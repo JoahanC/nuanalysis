3f504d1379fbcdd2bab22504530eb06c9851d329
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-11 03:40:59 -0700

    expanded scripts

--- bifrost_slidecell_8.py
+++ bifrost_slidecell_8.py
@@ -38,20 +38,3 @@ for idx in run_order:
             run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=18)
             run_object.sliding_cell_detection()
 
-run_order = {}
-counter = 0
-with open("../test/runlist_28.txt", 'r') as run_file:
-    run_data = run_file.readlines()
-for idx, datum in enumerate(run_data):
-    run_order[idx] = datum.split()
-
-for idx in run_order:
-    object_name = run_order[idx][0]
-    seqid = run_order[idx][1]
-    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/28/{seqid}/event_cl/"):
-        if f"5000_flag.txt" not in os.listdir(f"../bifrost_data/28/{seqid}/event_cl/"):
-            path = f"../bifrost_data/28/{seqid}/"
-            evdir = f"{path}event_cl/"
-            out_path = f"{path}products/"
-            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=28)
-            run_object.sliding_cell_detection()
