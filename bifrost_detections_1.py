9949711d3f88ffcc9b69b8ca061a1ff43317e919
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-12 07:13:32 -0700

    updated scripts

--- bifrost_detections_1.py
+++ bifrost_detections_1.py
@@ -3,6 +3,7 @@ from nuanalysis import NuAnalysis
 from tqdm import tqdm
 
 
+dtime = 1000
 run_order = {}
 counter = 0
 with open("../test/runlist_1.txt", 'r') as run_file:
@@ -13,12 +14,12 @@ for idx, datum in enumerate(run_data):
 for idx in tqdm(run_order):
     object_name = run_order[idx][0]
     seqid = run_order[idx][1]
-    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/1/{seqid}/event_cl/"):
-        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/1/{seqid}/event_cl/"):
+    if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/1/{seqid}/event_cl/"):
+        if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/1/{seqid}/event_cl/"):
             path = f"../bifrost_data/1/{seqid}/"
             evdir = f"{path}event_cl/"
             out_path = f"{path}products/"
-            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=1)
+            run_object = NuAnalysis(dtime, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=1)
             #run_object.detection_merging()
             run_object.write_net_detections()
 
@@ -32,12 +33,12 @@ for idx, datum in enumerate(run_data):
 for idx in tqdm(run_order):
     object_name = run_order[idx][0]
     seqid = run_order[idx][1]
-    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/11/{seqid}/event_cl/"):
-        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/11/{seqid}/event_cl/"):
+    if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/11/{seqid}/event_cl/"):
+        if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/11/{seqid}/event_cl/"):
             path = f"../bifrost_data/11/{seqid}/"
             evdir = f"{path}event_cl/"
             out_path = f"{path}products/"
-            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=11)
+            run_object = NuAnalysis(dtime, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=11)
             #run_object.detection_merging()
             run_object.write_net_detections()
 
@@ -51,12 +52,12 @@ for idx, datum in enumerate(run_data):
 for idx in tqdm(run_order):
     object_name = run_order[idx][0]
     seqid = run_order[idx][1]
-    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/21/{seqid}/event_cl/"):
-        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/21/{seqid}/event_cl/"):
+    if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/21/{seqid}/event_cl/"):
+        if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/21/{seqid}/event_cl/"):
             path = f"../bifrost_data/21/{seqid}/"
             evdir = f"{path}event_cl/"
             out_path = f"{path}products/"
-            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=21)
+            run_object = NuAnalysis(dtime, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=21)
             #run_object.detection_merging()
             run_object.write_net_detections()
 
--- bifrost_detections_8.py
+++ bifrost_detections_8.py
@@ -2,7 +2,7 @@ from helpers import *
 from nuanalysis import NuAnalysis
 from tqdm import tqdm
 
-
+dtime = 1000
 run_order = {}
 counter = 0
 with open("../test/runlist_8.txt", 'r') as run_file:
@@ -13,12 +13,12 @@ for idx, datum in enumerate(run_data):
 for idx in tqdm(run_order):
     object_name = run_order[idx][0]
     seqid = run_order[idx][1]
-    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/8/{seqid}/event_cl/"):
-        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/8/{seqid}/event_cl/"):
+    if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/8/{seqid}/event_cl/"):
+        if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/8/{seqid}/event_cl/"):
             path = f"../bifrost_data/8/{seqid}/"
             evdir = f"{path}event_cl/"
             out_path = f"{path}products/"
-            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=8)
+            run_object = NuAnalysis(dtime, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=8)
             #run_object.detection_merging()
             run_object.write_net_detections()
 
@@ -32,11 +32,11 @@ for idx, datum in enumerate(run_data):
 for idx in tqdm(run_order):
     object_name = run_order[idx][0]
     seqid = run_order[idx][1]
-    if f"5000_binning_flag.txt" in os.listdir(f"../bifrost_data/18/{seqid}/event_cl/"):
-        if f"5000_flag.txt" in os.listdir(f"../bifrost_data/18/{seqid}/event_cl/"):
+    if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/18/{seqid}/event_cl/"):
+        if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/18/{seqid}/event_cl/"):
             path = f"../bifrost_data/18/{seqid}/"
             evdir = f"{path}event_cl/"
             out_path = f"{path}products/"
-            run_object = NuAnalysis(5000, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=18)
+            run_object = NuAnalysis(dtime, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=18)
             run_object.write_net_detections()
 
