859c8dd06fd8b1d0160a8bb697ad7dc14a509658
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-12 16:19:43 -0700

    updated scripts

--- bifrost_bin_7.py
+++ bifrost_bin_7.py
@@ -21,22 +21,5 @@ for idx in run_order:
                 run_object = NuAnalysis(dtime, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=7)
                 run_object.event_extraction()
 
-run_order = {}
-counter = 0
-with open("../test/runlist_17.txt", 'r') as run_file:
-    run_data = run_file.readlines()
-for idx, datum in enumerate(run_data):
-    run_order[idx] = datum.split()
 
-for idx in run_order:
-    object_name = run_order[idx][0]
-    seqid = run_order[idx][1]
-    if f"nu{seqid}A01_cl.evt" in os.listdir(f"../bifrost_data/17/{seqid}/event_cl/"):
-        if f"nu{seqid}B01_cl.evt" in os.listdir(f"../bifrost_data/17/{seqid}/event_cl/"):
-            if f"{dtime}_binning_flag.txt" not in os.listdir(f"../bifrost_data/17/{seqid}/event_cl/"):
-                path = f"../bifrost_data/17/{seqid}/"
-                evdir = f"{path}event_cl/"
-                out_path = f"{path}products/"
-                run_object = NuAnalysis(dtime, 3, path=path, evdir=evdir, seqid=seqid, out_path=out_path, clean=True, bifrost=True, object_name=object_name, sessionid=17)
-                run_object.event_extraction()
 
