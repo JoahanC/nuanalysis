e6bb672776fa4fa229b4a1996f864833481322f3
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-17 07:12:13 -0700

    updated scripts again

--- bifrost_detections_11.py
+++ bifrost_detections_11.py
@@ -2,51 +2,54 @@ from helpers import *
 from nuanalysis import NuAnalysis
 from tqdm import tqdm
 
-dtime = 1000
-run_order = {}
-counter = 0
-low_phi_file = "./ref_files/nustar_pilow.txt"
-high_phi_file = "./ref_files/nustar_pihi.txt"
-starting_directory = os.getcwd()
-with open("../test/runlist_24.txt", 'r') as run_file:
-    run_data = run_file.readlines()
-for idx, datum in enumerate(run_data):
-    run_order[idx] = datum.split()
+dtimes = [5000, 1000, 500]
+for dtime in dtimes:
+    run_order = {}
+    counter = 0
+    low_phi_file = "./ref_files/nustar_pilow.txt"
+    high_phi_file = "./ref_files/nustar_pihi.txt"
+    starting_directory = os.getcwd()
+    with open("../test/runlist_24.txt", 'r') as run_file:
+        run_data = run_file.readlines()
+    for idx, datum in enumerate(run_data):
+        run_order[idx] = datum.split()
 
-for idx in tqdm(run_order):
-    object_name = run_order[idx][0]
-    seqid = run_order[idx][1]
-    if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/24/{seqid}/event_cl/"):
-        if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/24/{seqid}/event_cl/"):
-            path = f"../bifrost_data/24/{seqid}/"
-            evdir = f"{path}event_cl/"
-            out_path = f"{path}products/"
-            run_object = NuAnalysis(dtime, 3, path=path, low_phi_file=low_phi_file, high_phi_file=high_phi_file,
-                                    seqid=seqid, clean=True, bifrost=True, object_name=object_name)
-            run_object.write_net_detections()
-            run_object.recalculate_poisson()
-            os.chdir(starting_directory)
+    for idx in tqdm(run_order):
+        object_name = run_order[idx][0]
+        seqid = run_order[idx][1]
+        if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/24/{seqid}/event_cl/"):
+            if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/24/{seqid}/event_cl/"):
+                if f"{dtime}_poisson.tbl" not in os.listdir(f"../bifrost/24/{seqid}/detections"):
+                    path = f"../bifrost_data/24/{seqid}/"
+                    evdir = f"{path}event_cl/"
+                    out_path = f"{path}products/"
+                    run_object = NuAnalysis(dtime, 3, path=path, low_phi_file=low_phi_file, high_phi_file=high_phi_file,
+                                            seqid=seqid, clean=True, bifrost=True, object_name=object_name)
+                    run_object.write_net_detections()
+                    run_object.recalculate_poisson()
+                    os.chdir(starting_directory)
 
-run_order = {}
-counter = 0
-with open("../test/runlist_23.txt", 'r') as run_file:
-    run_data = run_file.readlines()
-for idx, datum in enumerate(run_data):
-    run_order[idx] = datum.split()
+    run_order = {}
+    counter = 0
+    with open("../test/runlist_23.txt", 'r') as run_file:
+        run_data = run_file.readlines()
+    for idx, datum in enumerate(run_data):
+        run_order[idx] = datum.split()
 
-for idx in tqdm(run_order):
-    object_name = run_order[idx][0]
-    seqid = run_order[idx][1]
-    if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/23/{seqid}/event_cl/"):
-        if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/23/{seqid}/event_cl/"):
-            path = f"../bifrost_data/23/{seqid}/"
-            evdir = f"{path}event_cl/"
-            out_path = f"{path}products/"
-            run_object = NuAnalysis(dtime, 3, path=path, low_phi_file=low_phi_file, high_phi_file=high_phi_file,
-                                    seqid=seqid, clean=True, bifrost=True, object_name=object_name)
-            run_object.write_net_detections()
-            run_object.recalculate_poisson()
-            os.chdir(starting_directory)
+    for idx in tqdm(run_order):
+        object_name = run_order[idx][0]
+        seqid = run_order[idx][1]
+        if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/23/{seqid}/event_cl/"):
+            if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/23/{seqid}/event_cl/"):
+                if f"{dtime}_poisson.tbl" not in os.listdir(f"../bifrost/23/{seqid}/detections"):
+                    path = f"../bifrost_data/23/{seqid}/"
+                    evdir = f"{path}event_cl/"
+                    out_path = f"{path}products/"
+                    run_object = NuAnalysis(dtime, 3, path=path, low_phi_file=low_phi_file, high_phi_file=high_phi_file,
+                                            seqid=seqid, clean=True, bifrost=True, object_name=object_name)
+                    run_object.write_net_detections()
+                    run_object.recalculate_poisson()
+                    os.chdir(starting_directory)
 
 
 
--- bifrost_detections_3.py
+++ bifrost_detections_3.py
@@ -2,49 +2,53 @@ from helpers import *
 from nuanalysis import NuAnalysis
 from tqdm import tqdm
 
-dtime = 1000
-run_order = {}
-counter = 0
-low_phi_file = "./ref_files/nustar_pilow.txt"
-high_phi_file = "./ref_files/nustar_pihi.txt"
-starting_directory = os.getcwd()
-with open("../test/runlist_3.txt", 'r') as run_file:
-    run_data = run_file.readlines()
-for idx, datum in enumerate(run_data):
-    run_order[idx] = datum.split()
 
-for idx in tqdm(run_order):
-    object_name = run_order[idx][0]
-    seqid = run_order[idx][1]
-    if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/3/{seqid}/event_cl/"):
-        if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/3/{seqid}/event_cl/"):
-            path = f"../bifrost_data/3/{seqid}/"
-            evdir = f"{path}event_cl/"
-            out_path = f"{path}products/"
-            run_object = NuAnalysis(dtime, 3, path=path, low_phi_file=low_phi_file, high_phi_file=high_phi_file,
-                                    seqid=seqid, clean=True, bifrost=True, object_name=object_name)
-            run_object.write_net_detections()
-            run_object.recalculate_poisson()
-            os.chdir(starting_directory)
+dtimes = [5000, 1000, 500]
+for dtime in dtimes:
+    run_order = {}
+    counter = 0
+    low_phi_file = "./ref_files/nustar_pilow.txt"
+    high_phi_file = "./ref_files/nustar_pihi.txt"
+    starting_directory = os.getcwd()
+    with open("../test/runlist_3.txt", 'r') as run_file:
+        run_data = run_file.readlines()
+    for idx, datum in enumerate(run_data):
+        run_order[idx] = datum.split()
 
-run_order = {}
-counter = 0
-with open("../test/runlist_13.txt", 'r') as run_file:
-    run_data = run_file.readlines()
-for idx, datum in enumerate(run_data):
-    run_order[idx] = datum.split()
+    for idx in tqdm(run_order):
+        object_name = run_order[idx][0]
+        seqid = run_order[idx][1]
+        if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/3/{seqid}/event_cl/"):
+            if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/3/{seqid}/event_cl/"):
+                if f"{dtime}_poisson.tbl" not in os.listdir(f"../bifrost/3/{seqid}/detections"):
+                    path = f"../bifrost_data/3/{seqid}/"
+                    evdir = f"{path}event_cl/"
+                    out_path = f"{path}products/"
+                    run_object = NuAnalysis(dtime, 3, path=path, low_phi_file=low_phi_file, high_phi_file=high_phi_file,
+                                            seqid=seqid, clean=True, bifrost=True, object_name=object_name)
+                    run_object.write_net_detections()
+                    run_object.recalculate_poisson()
+                    os.chdir(starting_directory)
 
-for idx in tqdm(run_order):
-    object_name = run_order[idx][0]
-    seqid = run_order[idx][1]
-    if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/13/{seqid}/event_cl/"):
-        if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/13/{seqid}/event_cl/"):
-            path = f"../bifrost_data/13/{seqid}/"
-            evdir = f"{path}event_cl/"
-            out_path = f"{path}products/"
-            run_object = NuAnalysis(dtime, 3, path=path, low_phi_file=low_phi_file, high_phi_file=high_phi_file,
-                                    seqid=seqid, clean=True, bifrost=True, object_name=object_name)
-            run_object.write_net_detections()
-            run_object.recalculate_poisson()
-            os.chdir(starting_directory)
+    run_order = {}
+    counter = 0
+    with open("../test/runlist_13.txt", 'r') as run_file:
+        run_data = run_file.readlines()
+    for idx, datum in enumerate(run_data):
+        run_order[idx] = datum.split()
+
+    for idx in tqdm(run_order):
+        object_name = run_order[idx][0]
+        seqid = run_order[idx][1]
+        if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/13/{seqid}/event_cl/"):
+            if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/13/{seqid}/event_cl/"):
+                if f"{dtime}_poisson.tbl" not in os.listdir(f"../bifrost/13/{seqid}/detections"):
+                    path = f"../bifrost_data/13/{seqid}/"
+                    evdir = f"{path}event_cl/"
+                    out_path = f"{path}products/"
+                    run_object = NuAnalysis(dtime, 3, path=path, low_phi_file=low_phi_file, high_phi_file=high_phi_file,
+                                            seqid=seqid, clean=True, bifrost=True, object_name=object_name)
+                    run_object.write_net_detections()
+                    run_object.recalculate_poisson()
+                    os.chdir(starting_directory)
 
