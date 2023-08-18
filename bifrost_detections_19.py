a040b4fca228196bf5d94a3caae93ce83d5658e6
Joahan Castaneda Jaimes <jcj@Joahans-MacBook-Pro.local>
2023-08-17 22:26:18 -0700

    redistribute scripts

--- bifrost_detections_19.py
+++ bifrost_detections_19.py
@@ -28,3 +28,26 @@ for dtime in dtimes:
                     run_object.write_net_detections()
                     run_object.recalculate_poisson()
                     os.chdir(starting_directory)
+
+
+    run_order = {}
+    counter = 0
+    with open("../test/runlist_18.txt", 'r') as run_file:
+        run_data = run_file.readlines()
+    for idx, datum in enumerate(run_data):
+        run_order[idx] = datum.split()
+
+    for idx in tqdm(run_order):
+        object_name = run_order[idx][0]
+        seqid = run_order[idx][1]
+        if f"{dtime}_binning_flag.txt" in os.listdir(f"../bifrost_data/18/{seqid}/event_cl/"):
+            if f"{dtime}_flag.txt" in os.listdir(f"../bifrost_data/18/{seqid}/event_cl/"):
+                if f"{dtime}_poisson.tbl" not in os.listdir(f"../bifrost_data/18/{seqid}/detections"):
+                    path = f"../bifrost_data/18/{seqid}/"
+                    evdir = f"{path}event_cl/"
+                    out_path = f"{path}products/"
+                    run_object = NuAnalysis(dtime, 3, path=path, low_phi_file=low_phi_file, high_phi_file=high_phi_file,
+                                            seqid=seqid, clean=True, bifrost=True, object_name=object_name)
+                    run_object.write_net_detections()
+                    run_object.recalculate_poisson()
+                    os.chdir(starting_directory)
